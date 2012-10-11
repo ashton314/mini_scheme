73e7744d13ccf2e100a86329d97e9a31803ac27a
Ashton Wiersdorf <aw8832@ipartner.net>
2012-10-11 10:27:28 -0600

    - Wrote a REPL to test with in scheme.pl. - Need to fix reading of single symbols on the REPL.

--- scheme.pl
+++ scheme.pl
@@ -2,5 +2,15 @@
 use strict;
 use warnings;
 
+use Data::Dumper;
+
 use Eval;
 use Analyze;
+use Reader;
+
+REPL: {
+    print "* ";
+    my $obj = scheme_read(0);
+    print Dumper($obj) . "\n";
+    redo REPL;
+}
