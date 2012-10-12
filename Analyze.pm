f607906f751c1d00e6c1f96a5fc268547412b3b4
Ashton Wiersdorf <aw8832@ipartner.net>
2012-10-11 22:58:19 -0600

    - Beginning work on Analyze.pm. - Still need to figure out how LAMBDA is going to work. Remember: I   need to implement closures.

--- Analyze.pm
+++ Analyze.pm
@@ -2,8 +2,71 @@ package Analyze;
 use strict;
 use warnings;
 
+use v5.10;
+use Scalar::Util;
+
 sub scheme_analyze {
-	my $expr = shift;
+    my $expr = shift;
+    
+    if (ref $expr eq 'ARRAY') {
+	given ($$expr[0]) {
+	    when ('string') { return sub { $$expr[1]; }; }
+	    when ('quote') { return sub { $$expr[1]; }; }
+	    when ('set!') {
+		my $var = $$expr[1];
+		my $val = scheme_analyze($$expr[2]);
+		return sub { 
+		    my $env = shift;
+		    return $$env{$var} = $val->($env);
+		}; }
+	    when ('define') {}
+	    when ('if') {
+		my $pred = scheme_analyze($$expr[1]);
+		my $tcl  = scheme_analyze($$expr[2]);
+		my $fcl  = defined($$expr[3]) ? scheme_analyze($$expr[3]) : 0;
+		return sub {
+		    my $env = shift;
+		    if ($pred->($env) ne 'nil') {
+			return $tcl->($env);
+		    }
+		    else {
+			if ($fcl) {
+			    return $fcl->($env);
+			}
+			else {
+			    return 'nil';
+			}
+		    }
+		};
+	    }
+	    when ('lambda') {
+		my @expression = @{ $expr };
+		my $params = shift @expression;
+		my $body = scheme_analyze(['begin', @expression]);
+
+		# WORKING HERE
+
+	    }
+
+	    when ('begin') {}
+	    default {}
+	}
+    }
+    else {
+	if (looks_like_number($expr)) {
+	    return sub {
+		return $expr;
+	    };
+	}
+	else {		# Variable
+	    return sub {
+		my $env = shift;
+		return $$env{$expr};
+	    };
+	}
+    }
 }
+
+
 
 1;
