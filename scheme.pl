4196088aec5af849066f0a63a7196fdf41645ec1
Ashton Wiersdorf <aw8832@ipartner.net>
2012-10-15 14:02:18 -0600

    - Something is wrong with the reader. I'm going to have to make   scheme_read return two values.

--- scheme.pl
+++ scheme.pl
@@ -9,116 +9,136 @@ use Scalar::Util;
 use Reader;
 
 my %TRACED_FUNCTIONS = ();
-my $GLOBAL_ENV = {
-		  parent_env => undef,
-		  env => {
-			  quit => {
-				   closure_env => {},
-				   args        => [],
-				   lambda_expr => undef,
-				   body        => sub {
-				       print "Happy Happy Joy Joy\n";
-				       exit;
-				   },
-				  },
-			  apply => {
-				    closure_env => {},
-				    args        => ['function', 'args'],
-				    lambda_expr => undef,
-				    body        => sub {
-					my $env = shift;
-					my $func = find_var('function', $env);
-					my $args = find_var('args', $env);
-				    },
-				   },
-			  '=' => {
-				  closure_env => {},
-				  args        => ['.', 'args'],
-				  lambda_expr => undef,
-				  body => sub {
-				      my $env = shift;
-				      my @args = @{ find_var('args', $env) };
-				      (print "ERROR: Got @{ [scalar @args] } args and expected at least 2 -- =\n" && return undef) if scalar @args < 2;
-				      my $thing = shift @args;
-				      foreach (@args) {
-					  return '#f' if $_ != $thing;
-				      }
-				      return '#t';
-				  },
-				 },
-			  '+' => {
-				  closure_env => {},
-				  args        => ['.', 'args'],
-				  lambda_expr => undef,
-				  body => sub {
-				      my $env = shift;
-				      my $args = find_var('args', $env);
-				      my $sum = 0;
-				      map { $sum += $_ } @{ $args };
-				      return $sum;
-				  },
-				 },
-			  '-' => {
-				  closure_env => {},
-				  args        => ['.', 'args'],
-				  lambda_expr => undef,
-				  body => sub {
-				      my $env = shift;
-				      my $args = find_var('args', $env);
-				      my $sum = 0;
-				      map { $sum -= $_ } @{ $args };
-				      return $sum;
-				  },
-				 },
-			  '*' => {
-				  closure_env => {},
-				  args        => ['.', 'args'],
-				  lambda_expr => undef,
-				  body => sub {
-				      my $env = shift;
-				      my $args = find_var('args', $env);
-				      my $prod = 1;
-				      map { $prod *= $_ } @{ $args };
-				      return $prod;
-				  },
-				 },
-			  '/' => {
-				  closure_env => {},
-				  args        => ['.', 'args'],
-				  lambda_expr => undef,
-				  body => sub {
-				      my $env = shift;
-				      my $args = find_var('args', $env);
-				      my $quot = 1;
-				      map { $quot /= $_ } @{ $args };
-				      return $quot;
-				  },
-				 },
-			  'mod' => {
-				  closure_env => {},
-				  args        => ['.', 'args'],
-				  lambda_expr => undef,
-				  body => sub {
-				      my $env = shift;
-				      my $args = find_var('args', $env);
-				      my $rem = 0;
-				      map { $rem %= $_ } @{ $args };
-				      return $rem;
-				  },
-				 },
-			  trace => {
-				    closure_env => {},
-				    args        => ['symbol'],
-				    lambda_expr => undef,
-				    body => sub {
-					my $env = shift;
-					my $func = find_var('symbol', $env);
-					$TRACED_FUNCTIONS{$func} = 1;
-					return "OK";
-				    },
-				   },
-			 },
-		 };
+my %GLOBAL_ENV = (
+    parent_env => undef,
+    env => {
+	quit => {
+	    closure_env => {},
+	    args        => [],
+	    lambda_expr => undef,
+	    body        => sub {
+		print "Happy Happy Joy Joy\n";
+		exit;
+	    },
+	},
+	apply => {
+	    closure_env => {},
+	    args        => ['function', 'args'],
+	    lambda_expr => undef,
+	    body        => sub {
+		my $env = shift;
+		my $func = find_var('function', $env);
+		my $args = find_var('args', $env);
+	    },
+	},
+	'=' => {
+	    closure_env => {},
+	    args        => ['.', 'args'],
+	    lambda_expr => undef,
+	    body => sub {
+		my $env = shift;
+		my @args = @{ find_var('args', $env) };
+		(print "ERROR: Got @{ [scalar @args] } args and expected at least 2 -- =\n" && return undef) if scalar @args < 2;
+		my $thing = shift @args;
+		foreach (@args) {
+		    return '#f' if $_ != $thing;
+		}
+		return '#t';
+	    },
+	},
+	'+' => {
+	    closure_env => {},
+	    args        => ['.', 'args'],
+	    lambda_expr => undef,
+	    body => sub {
+		my $env = shift;
+		my $args = find_var('args', $env);
+		my $sum = 0;
+		map { $sum += $_ } @{ $args };
+		return $sum;
+	    },
+	},
+	'-' => {
+	    closure_env => {},
+	    args        => ['.', 'args'],
+	    lambda_expr => undef,
+	    body => sub {
+		my $env = shift;
+		my $args = find_var('args', $env);
+		my $sum = 0;
+		map { $sum -= $_ } @{ $args };
+		return $sum;
+	    },
+	},
+	'*' => {
+	    closure_env => {},
+	    args        => ['.', 'args'],
+	    lambda_expr => undef,
+	    body => sub {
+		my $env = shift;
+		my $args = find_var('args', $env);
+		my $prod = 1;
+		map { $prod *= $_ } @{ $args };
+		return $prod;
+	    },
+	},
+	'/' => {
+	    closure_env => {},
+	    args        => ['.', 'args'],
+	    lambda_expr => undef,
+	    body => sub {
+		my $env = shift;
+		my $args = find_var('args', $env);
+		my $quot = 1;
+		map { $quot /= $_ } @{ $args };
+		return $quot;
+	    },
+	},
+	'mod' => {
+	    closure_env => {},
+	    args        => ['.', 'args'],
+	    lambda_expr => undef,
+	    body => sub {
+		my $env = shift;
+		my $args = find_var('args', $env);
+		my $rem = 0;
+		map { $rem %= $_ } @{ $args };
+		return $rem;
+	    },
+	},
+	closure_env => {
+	    closure_env => {},
+	    args        => ['symbol'],
+	    lambda_expr => undef,
+	    body => sub {
+		my $env = shift;
+		my $obj = find_var('symbol', $env);
+		if (ref $obj eq 'HASH') {
+		    print Dumper($$obj{closure_env}) . "\n";
+		    return undef;
+		}
+		else {
+		    print "Not a function.\n";
+		    return undef;
+		}
+	    },
+	},
+	trace => {
+	    closure_env => {},
+	    args        => ['symbol'],
+	    lambda_expr => undef,
+	    body => sub {
+		my $env = shift;
+		my $func = find_var('symbol', $env);
+		$TRACED_FUNCTIONS{$func} = (exists $TRACED_FUNCTIONS{$func} &&
+					    $TRACED_FUNCTIONS{$func}) ? 0 : 1;
+		print $TRACED_FUNCTIONS{$func} ? "TRACED\n" : "UNTRACED\n";
+		return undef;
+	    },
+	},
+    },
+    );
+
 
 REPL: {
     print "* ";
@@ -127,7 +147,8 @@ REPL: {
 	$expr = scheme_read(0, "\n");
 	redo INPUT unless defined($expr);
     }
-    print "\n" . scheme_eval($expr, $GLOBAL_ENV) . "\n";
+    my $to_print = scheme_eval($expr, \%GLOBAL_ENV);
+    print "\n" . (defined($to_print) ? $to_print : ";UNDEF") . "\n";
     redo REPL;
 }
 
@@ -153,9 +174,7 @@ sub scheme_analyze {
 		my $var = $$expr[1];
 		my $val = scheme_analyze($$expr[2]);
 		return sub { 
-		    my $env = shift;
-		    my $inner_env = $$env{env};
-		    return ($$inner_env{$var} = $val->($env));
+		    set_var($var, $_[0], $val->($_[0]));
 		}; }
 	    when ('define') {
 		my @expr = @{ $expr };
@@ -165,13 +184,21 @@ sub scheme_analyze {
 		if (ref $expr[1] eq 'ARRAY') { # Function def
 		    my @arglist    = @{ shift @expression };
 		    my @body       = @expression;
-		    my $to_analyze = ['set!', (shift @arglist),
-					   ['lambda', \@arglist, @body]];
-		    return scheme_analyze($to_analyze);
+		    my $to_call =
+		      scheme_analyze(['set!', (shift @arglist),
+				      ['lambda', \@arglist, @body]]);
+		    return sub {
+			$_[0]->{env}{$expr[1][0]} = '';
+			$to_call->($_[0]);
+		    };
 		}
 		else {		# Var def
-		    return scheme_analyze(['set!', (shift @expression),
-					   (shift @expression)]);
+		    my $to_call = scheme_analyze(['set!', (shift @expression),
+						  (shift @expression)]);
+		    return sub {
+			$_[0]->{env}{$expr[1]} = '';
+			$to_call->($_[0]);
+		    };
 		}
 	    }
 	    when ('if') {
@@ -179,13 +206,12 @@ sub scheme_analyze {
 		my $tcl  = scheme_analyze($$expr[2]);
 		my $fcl  = defined($$expr[3]) ? scheme_analyze($$expr[3]) : 0;
 		return sub {
-		    my $env = shift;
-		    if ($pred->($env) ne '#f') {
-			return $tcl->($env);
+		    if ($pred->($_[0]) ne '#f') {
+			return $tcl->($_[0]);
 		    }
 		    else {
 			if ($fcl) {
-			    return $fcl->($env);
+			    return $fcl->($_[0]);
 			}
 			else {
 			    return 'nil';
@@ -197,20 +223,17 @@ sub scheme_analyze {
 		my @block = @{ $expr };
 		shift @block;	# Cut off the 'BEGIN'
 		my @exprs = map { scheme_analyze($_) } @block;
-		my $first_exp = pop @exprs;
-		my $seq = sub {
-		    my $env = shift;
-		    $first_exp->($env);
+		return sub {
+		    while (@exprs) {
+			my $expr = shift @exprs;
+			if (@exprs) {
+			    $expr->($_[0]);
+			}
+			else {
+			    return $expr->($_[0]);
+			}
+		    }
 		};
-		while (@exprs) {
-		    my $exp = pop @exprs;
-		    $seq = sub {
-			my $env = shift;
-			$exp->($env);
-			$seq->($env);
-		    };
-		}
-		return $seq;
 	    }
 	    when ('lambda') {
 		my @expression = @{ $expr };
@@ -237,11 +260,10 @@ sub scheme_analyze {
 		  map { scheme_analyze($_) } @expression;
 
 		return sub {
-		    my $env = shift;
-		    my %func = %{ $func_proc->($env) };
+		    my %func = %{ $func_proc->($_[0]) };
 		    my @arg_syms = @{ $func{args} };
 		    my @arg_vals = ();
-		    my @arg_vals = map { $_->($env) } @arg_procs;
+		    my @arg_vals = map { $_->($_[0]) } @arg_procs;
 		    my @arg_vals_copy = @arg_vals;
 		    my %arg_hash = ();
 		    my $slurpy = 0;
@@ -258,15 +280,11 @@ sub scheme_analyze {
 			    $arg_hash{$sym} = shift @arg_vals;
 			}
 		    }
-		    my $nenv = {
-				parent_env => {
-					       parent_env => $env,
-					       env       => $func{closure_env},
-					      },
-				env        => \%arg_hash,
-			       };
-#		    print "Constructed env: " . Dumper($nenv) . "\n";
-		    if (exists $TRACED_FUNCTIONS{$expression[0]}) {
+		    my $nenv = merge_envs($func{closure_env},
+					  \%arg_hash);
+
+		    if (exists $TRACED_FUNCTIONS{$expression[0]} &&
+		       $TRACED_FUNCTIONS{$expression[0]}) {
 			print "CALLING FUNCTION: @{ [$expression[0]] }\n";
 			print "            ARGS: @arg_vals_copy\n";
 		    }
@@ -283,22 +301,23 @@ sub scheme_analyze {
 	}
 	else {		# Variable
 	    return sub {
-		my $env = shift;
-#		print Dumper($env) . "\n";
-#		print STDERR "Looking for $expr";
-		return find_var($expr, $env);
+		return find_var($expr, $_[0]);
 	    };
 	}
     }
 }
 
+sub merge_envs {
+    my ($parent, $new) = @_;
+    return {
+	    parent_env => $parent,
+	    env        => $new };
+}
+
 sub find_var {
     my ($var, $env) = @_;
-#    print STDERR ".";
     my $this_env = $$env{env};
     if (exists $$this_env{$var}) {
-	# print STDERR "Found: " . $$this_env{$var} . "\n";
-	# select undef, undef, undef, .5;
 	return $$this_env{$var};
     }
     else {
@@ -307,6 +326,23 @@ sub find_var {
 	    return find_var($var, $parent_env);
 	}
 	else {
+	    return undef;
+	}
+    }
+}
+
+sub set_var {			# setf
+    my ($var, $env, $val) = @_;
+    my $this_env = $$env{env};
+    if (exists $$this_env{$var}) {
+	return $$env{env}->{$var} = $val;
+    }
+    else {
+	if ($$env{parent_env}) {
+	    return set_var($var, $$env{parent_env}, $val);
+	}
+	else {
+	    print STDERR "ERROR: VAR $var NOT DEFINED\n";
 	    return undef;
 	}
     }
