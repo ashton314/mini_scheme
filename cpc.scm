6421878ef84e3ff40ebecea80340fe2103c151bc
Ashton Wiersdorf <ashton.wiersdorf@mailblock.net>
2013-03-15 13:03:50 -0600

    - CPS-transformation is mostly working. There are still unresolved   issues with functions such as SET!, READ, etc. - I need to clean up the implementation of strings. There's a bug in   some cases when a "." appears in the string... I think.

--- cpc.scm
+++ cpc.scm
@@ -52,16 +52,10 @@
        ,cont))
    (gensym)))
 
-;; (begin
-;;   (+ a b)
-;;   (+ c d))
-
-;; ((lambda (cnt) (+ a b) (cnt)) (lambda (cnt2) (+ c d) (cnt2)))
-
 (define (cpc-begin sexpr env cont)
   (define (loop exprs)
-    (cpc (car exprs) env (if (cdr exprs) (loop (cdr exprs)) cont)))
-  (loop (reverse (cdr sexpr))))    
+    (cpc (car exprs) env (if (cdr exprs) (list 'lambda (list (gensym)) (loop (cdr exprs))) cont)))
+  (loop (cdr sexpr)))    
 
 (define (cpc-catch sexpr env cont)
   ((lambda (en)
@@ -117,17 +111,17 @@
 				       (cons (car x) z)))))))
 	  (loop1 sexpr nil nil)))
 
-;; (define-macro (mcp func)		; Make Contiuation Passing
-;;   `(define ,(implode (cons '@ (explode func)))
-;;      (lambda (arg1 &rest args)
-;;        (set! args (cons arg1 args))
-;;        (let ((cont (car (reverse args))))
-;; 	 (cont (apply ,func (reverse (cdr (reverse args)))))))))
+(define-macro (mcp func)		; Make Contiuation Passing
+  `(define ,(implode (cons '@ (explode func)))
+     (lambda (arg1 &rest args)
+       (set! args (cons arg1 args))
+       (let ((cont (car (reverse args))))
+	 (cont (apply ,func (reverse (cdr (reverse args)))))))))
 
-;; (define-macro (mcps . funcs)
-;;   `(begin ,@(map (lambda (func) `(mcp ,func)) funcs)))
+(define-macro (mcps . funcs)
+  `(begin ,@(map (lambda (func) `(mcp ,func)) funcs)))
 
-;(mcps > < = + - * / mod eq?) ; quit car cdr last rplaca rplacd cons list
-      ;; not number? macro? list? apply null? int read clear time load write
-      ;; write-string write-string-err error write-err sleep terpri terpri-err
-      ;; fle macroexpand implode explode gensym env_symbols verbose dumper trace)
+(mcps > < = + - * / mod eq? quit car cdr last rplaca rplacd cons list
+      not number? macro? list? apply null? int read clear time load write
+      write-string write-string-err error write-err sleep terpri terpri-err
+      fle macroexpand implode explode gensym env_symbols verbose dumper trace)
